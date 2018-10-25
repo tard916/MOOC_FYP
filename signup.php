5e619f6a54469a23df0f5306cc08de6537655d1e
Rahimi Diallo <tard916@outlook.com>
2018-10-26 05:34:17 +0800

    Update signup.php

---
+++ signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sign Up </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/custome.css" rel="stylesheet">
+
+    <!--This is the google Arvo font and font-awesome-->
+    <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+    
+</body>
+</html>
