7e3e099d52d71b169e61bc027882f89c224a0aba
Rahimi Diallo <tard916@outlook.com>
2018-11-01 00:03:36 +0800

    Navbar fix
    cloning the index navbar for the rest of pages
    
    Co-Authored-By: mehrabkamrani <mehrabkamrani@users.noreply.github.com>

--- becomeaninstrutor.php
+++ becomeaninstrutor.php
@@ -10,18 +10,18 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/custome.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 
     <!--This is the google Arvo font and font-awesome-->
     <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
     <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
-    <header>    
-        <!-- this block is for the navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light navbar-dark bg-primary">
+    <header> 
+                <!-- this block is for the navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <!-- Navbar content -->
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand active" href="index.php">
                 <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
                 HELP-MOOC
             </a>
@@ -30,10 +30,10 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="nav navbar-nav pull-sm-left">                
+                <ul class="navbar-nav mx-auto">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="navbar-toggler-icon"></span>
+                            <i class="fa fa-list-ul"></i>
                             Categories
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -43,27 +43,34 @@
                             <a class="dropdown-item" href="#">Something else here</a>
                         </div>
                     </li>
-                    <li class="nav-item ">
-                        <a class="nav-link active" href="becomeaninstrutor.php">Become an instructor <span class="sr-only">(current)</span></a>
-                    </li>            
+
                 </ul>
 
-                <ul class="navbar-nav mx-auto">
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+                <ul class="navbar-nav mx-auto col-lg-4  mt-2 mt-lg-0">
+
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Search for Courses" aria-label="Recipient's username" aria-describedby="button-addon2">
+                      <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa fa-search"></i></button>
+                      </div>
+                    </div>
+                </ul>
+
+                <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+                    <li class="nav-item ">
+                        <a class="btn btn-danger" href="becomeaninstrutor.php">Become an instrutor <span class="sr-only">(current)</span></a>
+                    </li>
                 </ul>
 
                 <ul class="nav navbar-nav pull-sm-right">
-                    <li class="nav-item">
+                    <li class="nav-item mr-lg-1">
                         <a class="nav-link" href="login.php">Log In</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link " href="signup.php">Sign Up</a>
+                        <a class="btn btn-outline-primary" href="signup.php">Sign Up</a>
                     </li>
                 </ul>
-                
+
             </div>
         </nav>
     </header>
