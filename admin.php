585a31b480a64f874b02bf98a66973f0641882cd
mehrab <mk.zizou@gmail.com>
2019-02-13 14:11:35 +0800

    Add admin page

---
+++ admin.php
@@ -0,0 +1,69 @@
+<?php
+    include('./backend/classes/DB.php');
+    include('./backend/classes/Login.php');
+    if (Login::isLoggedIn()) {
+      //echo 'Logged In!';
+      //echo Login::isLoggedIn();
+      $outputkey  = Login::isLoggedIn();
+      list($user, $key) = explode("_", $outputkey);
+
+      if ($user == 'INS') {
+        $user_Name = DB::query('SELECT instructor_name FROM instructor WHERE ins_uniquID=:outputkey', array(':outputkey'=>$outputkey))[0]['instructor_name'];
+
+        include('./instructorHeader.php');
+      }
+    }else {
+        include('../mainHeader.php');
+    }
+?>
+    <main role="main" class="container my-courses-wrapper">
+        <h1 class="mt-4">Pending Courses</h1>
+        <hr>
+
+        <div class="row">
+
+            <?php
+                $select_course = DB::query('SELECT * FROM course WHERE instructor_id =:outputkey', array(':outputkey'=>$outputkey));
+                foreach ($select_course as $value) {
+                    $imagePath = $value['course_path_fol'].'/'.$value['course_image'];
+                    //echo $imagePath;
+                    //newCourseCurriculum.php?cid=<?php echo $value['crs_uniqueID'];
+            ?>
+                <div class="col-md-4 col-sm-6 mb-4">
+                    <a href="adminCourse.php?cid=<?php echo $value['crs_uniqueID']?>">
+                        <div class="card">
+                            <img class="card-img-top" src="<?php echo $imagePath;?>" alt="Card image cap">
+                            <div class="card-body pb-0">
+                                <h5 class="card-title mb-4"><?php echo $value['course_name'];?></h5>
+                                <?php
+                                    $ins_uniquID = $value['instructor_id'];
+                                    $retrieve_the_instructor = DB::query('SELECT instructor_name FROM instructor where ins_uniquID= :ins_uniquID',array(':ins_uniquID'=>$ins_uniquID))[0]['instructor_name'];
+                                ?>
+                                <h6 class="card-subtitle mb-4 text-muted"><?php echo $retrieve_the_instructor;?></h6>
+                                <p>Rating: 0.0 <small class="text-muted">(0)</small><small class="text-muted pull-right mt-1"><?php echo $value['duration'];?> weeks</small></p>
+
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php
+                }
+            ?>
+        </div>
+    </main>
+     <!-- This part is for the Footer.-->
+     <footer class="footer py-4 bg-dark text-light">
+         <div class="container">
+         <span class="cprTxt"> &copy;2018 HELP-MOOC</span>
+         </div>
+     </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ adminCourse.php
@@ -0,0 +1,151 @@
+<?php
+  include('./backend/classes/DB.php');
+  include('./backend/classes/Login.php');
+   $courseID = $_GET['cid'];
+  // $courseID = 'CRS5c4eb123cbbbe9.91086078';
+  if (Login::isLoggedIn()) {
+    //echo 'Logged In!';
+    //echo Login::isLoggedIn();
+    $outputkey  = Login::isLoggedIn();
+    echo "<script>console.log( 'Debug Objects: " . $outputkey . "' );</script>";
+    list($user, $key) = explode("_", $outputkey);
+    echo "<script>console.log( 'user_type: " . $user . "' );</script>";
+
+
+    if ($user == 'INS') {
+      $user_Name = DB::query('SELECT instructor_name FROM instructor WHERE ins_uniquID=:outputkey', array(':outputkey'=>$outputkey))[0]['instructor_name'];
+
+      include('./instructorHeader.php');
+      echo "<script>console.log( 'Debug Objects: " . $user_Name . "' );</script>";
+    }
+    /*if ($user == 'STD') {
+      $user_Name = DB::query('SELECT student_name FROM student WHERE std_uniquID=:outputkey', array(':outputkey'=>$outputkey))[0]['student_name'];
+      include('./userHeader.php');
+      echo "<script>console.log( 'Debug Objects: " . $user_Name . "' );</script>";
+    }*/
+  }else {
+      include('mainHeader.php');
+  }
+
+  $courseResult = DB::query('SELECT * FROM course WHERE crs_uniqueID =:courseID', array(':courseID'=>$courseID));
+
+
+?>
+
+<main role="main" class="course-page">
+
+    <!--This part is for the Carousel slide -->
+    <div class="course-jumbotron jumbotron m-0 text-light">
+        <div class="container">
+            <?php
+                foreach ($courseResult as $value) {
+                    $instructorId = $value['instructor_id'];
+                    $intructorName = DB::query('SELECT * FROM instructor WHERE  ins_uniquID =:ins_uniquID', array(':ins_uniquID'=>$instructorId))[0]['instructor_name'];
+            ?>
+            <!--<h1 class="display-4 text-center mb-3"></h1>
+            <p class="lead text-center px-sm-3 px-md-5">
+                <span class="pull-left"><i class="fa fa-star-o" aria-hidden="true"></i> 4.50/5.00 <small>(100)</small></span>
+                <span class=""><i class="fa fa-user-o" aria-hidden="true"></i> <?php echo $intructorName;?></span>
+                <span class="pull-right"><i class="fa fa-clock-o" aria-hidden="true"></i> </span>
+
+            </p>-->
+
+            <h1 class="display-4 text-center mb-3"><?php echo $value['course_name'];?></h1>
+            <div class="row lead text-center">
+                <div class="col-sm-6"><i class="fa fa-user-o" aria-hidden="true"></i> <?php echo $intructorName;?></div>
+                <div class="col-sm-6"><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo $value['duration'];?> Weeks</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <a class="btn btn-outline-success btn-block mt-5" href="" role="button"><i class="fa fa-check" aria-hidden="true"></i> Approve</a>
+                </div>
+                <div class="col-sm-6">
+                    <a class="btn btn-outline-danger btn-block mt-5" href="" role="button"><i class="fa fa-times" aria-hidden="true"></i> Reject</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--This part is for the Carousel slide -->
+    <!-- List group -->
+    <div class="list-group text-center" id="course-sections" role="tablist">
+      <a class="list-group-item list-group-item-action active" data-toggle="list" href="#overview" role="tab">Overview</a>
+      <a class="list-group-item list-group-item-action" data-toggle="list" href="#syllabus" role="tab">Syllabus</a>
+    </div>
+
+    <!-- Tab panes -->
+    <div class="container">
+        <div class="tab-content">
+            <div class="tab-pane fade py-4 show active" id="overview" role="tabpanel">
+                <div class="course-section">
+                    <h4>Description</h4>
+                    <hr>
+                    <p><?php echo $value['descriptions'];?></p>
+                </div>
+                <div class="course-section">
+                    <h4>Learning Outcome</h4>
+                    <hr>
+                    <p><?php echo $value['learning_outcomes'];?></p>
+                </div>
+                <div class="course-section">
+                    <h4>Requirements</h4>
+                    <hr>
+                    <p><?php echo $value['pre_requirments'];?></p>
+                </div>
+            </div>
+    <?php
+        }
+    ?>
+            <div class="tab-pane fade py-4" id="syllabus" role="tabpanel">
+                <div class="course-curriculum-section">
+
+                    <?php
+                        $courseDuration = DB::query('SELECT duration FROM course WHERE crs_uniqueID =:courseID', array(':courseID'=>$courseID))[0]['duration'];
+                        for($i = 1; $i<=$courseDuration; $i++){
+                    ?>
+
+                    <h5 class="">Week <?php echo $i;?></h5>
+                    <ul class="list-unstyled mb-5">
+                    <?php
+                        $crriculum = DB::query('SELECT * FROM course_cirriculum WHERE week_number = :id AND course_id= :courseID', array(':id'=>$i, ':courseID'=>$courseID));
+                        foreach ($crriculum as $value) {
+                    ?>
+
+                    <li class=""><a class="curriculum-link text-dark" href="curriculumPlayer.php?crs_UniqueID=<?php echo $courseID;?>"><i class="fa fa-file-video-o" aria-hidden="true"></i> <?php echo $value['title'];?></a></li>
+                        <?php
+                            }
+                        ?>
+                        <!--<li class=""><a class="curriculum-link text-dark" href="#"><i class="fa fa-file-video-o" aria-hidden="true"></i> Model View Controler (MVC) with PHP part 1</a></li>
+                        <li class=""><a class="curriculum-link text-dark" href="#"><i class="fa fa-file-text-o" aria-hidden="true"></i> Intro to MVC</a></li>-->
+                    </ul>
+                <?php
+                    }
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</main>
+
+  <footer class="footer py-4 bg-dark text-light">
+      <div class="container">
+      <span class="cprTxt"> &copy;2018 HELP-MOOC</span>
+      </div>
+  </footer>
+  <!--This part is for the footer section -->
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug
+    <script src="js/ie10-viewport-bug-workaround.js"></script> -->
+</body>
+</html>
