6659b69a906dc42c06a7416f0222c70e038a785f
Rahimi Diallo <tard916@outlook.com>
2019-03-19 23:13:03 +0800

    Update ckle.php

--- backend/ckle.php
+++ backend/ckle.php
@@ -7,21 +7,46 @@ date_default_timezone_set("Asia/Kuala_Lumpur");
 // console.log($_POST["userID"]);
 // </script>';
 if(isset($_GET['userID'])){
-    $today_date=date("Y-m-d H:i:s");
+    $today_date=date("Y-m-d");
     $stdID = $_GET['userID'];
+    $iniActD= 1;
+    
+    // $cDate = new DateTime($today_date);
+    // $lastLogin = DB::query('SELECT * FROM student_logs WHERE std_uniqueID=:id', array(':id'=>$stdID))[0]['last_login'];
+    // $ltLogin = new DateTime($lastLogin);
+
+    // if($ltLogin < $cDate){
+    //     echo 'less';
+    // }else if($ltLogin == $cDate){
+    //     echo '=';
+    // }
 
     //echo $stdID;
 
     if (DB::query('SELECT * FROM student_logs WHERE std_uniqueID=:id', array(':id'=>$stdID))) {
 
-        DB::query('UPDATE student_logs SET last_login= :last_login WHERE std_uniqueID= :std_uniqueID',
-        array(':last_login'=>$today_date,':std_uniqueID'=>$stdID));
-        header('Location: ../studentCourses.php');
+        // $lastLogin = DB::query('SELECT * FROM student_logs WHERE std_uniqueID=:id', array(':id'=>$stdID))[0]['last_login'];
+        $active_days = DB::query('SELECT * FROM student_logs WHERE std_uniqueID=:id', array(':id'=>$stdID))[0]['active_days'];
+        $lastLogin = DB::query('SELECT * FROM student_logs WHERE std_uniqueID=:id', array(':id'=>$stdID))[0]['last_login'];
+        $ltLogin = new DateTime($lastLogin);
+        $cDate = new DateTime($today_date);
+
+        if($ltLogin ==$cDate){
+            DB::query('UPDATE student_logs SET last_login= :last_login, active_days= :active_days WHERE std_uniqueID= :std_uniqueID',
+            array(':last_login'=>$today_date,':active_days'=>$active_days,':std_uniqueID'=>$stdID));
+            header('Location: ../studentCourses.php');
+        }else{
+            $actD = $active_days + 1;
+            DB::query('UPDATE student_logs SET last_login= :last_login, active_days= :active_days WHERE std_uniqueID= :std_uniqueID',
+            array(':last_login'=>$today_date,':active_days'=>$actD,':std_uniqueID'=>$stdID));
+            header('Location: ../studentCourses.php');
+        }
+        
     }else {
         
-        DB::query('INSERT INTO student_logs VALUES ( \'\',:last_login, :std_uniqueID )',
-        array(':last_login'=>$today_date,':std_uniqueID'=>$stdID));
-        header('Location: ../index.php');
+        DB::query('INSERT INTO student_logs VALUES ( \'\',:last_login, :std_uniqueID, :active_days)',
+        array(':last_login'=>$today_date,':std_uniqueID'=>$stdID, ':active_days'=>$iniActD));
+        header('Location: ../studentCourses.php');
     }
 
 }
