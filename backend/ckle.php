2e6f3acdb47c89354de8465c31f909dc9a997b38
Rahimi Diallo <tard916@outlook.com>
2019-04-07 16:40:32 +0800

    Update ckle.php

--- backend/ckle.php
+++ backend/ckle.php
@@ -6,7 +6,7 @@ date_default_timezone_set("Asia/Kuala_Lumpur");
 // echo '<script>
 // console.log($_POST["userID"]);
 // </script>';
-if(isset($_GET['userID'])){
+if(isset($_GET['userID'])){ 
     $today_date=date("Y-m-d");
     $stdID = $_GET['userID'];
     $crsID = $_GET['courseID'];
@@ -21,21 +21,23 @@ if(isset($_GET['userID'])){
         $cDate = new DateTime($today_date);
 
         if($ltLogin ==$cDate){
-            DB::query('UPDATE student_logs SET last_login= :last_login, active_days= :active_days WHERE std_uniqueID= :std_uniqueID',
-            array(':last_login'=>$today_date,':active_days'=>$active_days,':std_uniqueID'=>$stdID));
-            header('Location: ../studentCourses.php');
+            DB::query('UPDATE student_logs SET last_login= :last_login, active_days= :active_days WHERE std_uniqueID= :std_uniqueID 
+            AND course_ID= :course_ID',
+            array(':last_login'=>$today_date,':active_days'=>$active_days,':std_uniqueID'=>$stdID, ':course_ID'=>$crsID));
+            header('Location: ../joinedCourse.php?crs_UniqueID='.$crsID);
         }else{
             $actD = $active_days + 1;
-            DB::query('UPDATE student_logs SET last_login= :last_login, active_days= :active_days WHERE std_uniqueID= :std_uniqueID',
-            array(':last_login'=>$today_date,':active_days'=>$actD,':std_uniqueID'=>$stdID));
-            header('Location: ../studentCourses.php');
+            DB::query('UPDATE student_logs SET last_login= :last_login, active_days= :active_days WHERE std_uniqueID= :std_uniqueID 
+            AND course_ID= :course_ID',
+            array(':last_login'=>$today_date,':active_days'=>$actD,':std_uniqueID'=>$stdID,':course_ID'=>$crsID));
+            header('Location: ../joinedCourse.php?crs_UniqueID='.$crsID);
         }
         
     }else {
         
-        DB::query('INSERT INTO student_logs VALUES ( \'\',:last_login, :std_uniqueID, :active_days)',
-        array(':last_login'=>$today_date,':std_uniqueID'=>$stdID, ':active_days'=>$iniActD));
-        header('Location: ../studentCourses.php');
+        DB::query('INSERT INTO student_logs VALUES ( \'\',:last_login, :std_uniqueID, :course_ID, :active_days)',
+        array(':last_login'=>$today_date,':std_uniqueID'=>$stdID, ':active_days'=>$iniActD, ':course_ID'=>$crsID));
+        header('Location: ../joinedCourse.php?crs_UniqueID='.$crsID);
     }
 
 }
